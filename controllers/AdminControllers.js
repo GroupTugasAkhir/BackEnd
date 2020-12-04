e950e5005e39d987ebc5f16842d2bb3c602648e2
hasianamin <hasianamin14@gmail.com>
2020-12-04 22:18:44 +0700

    add controller to get category

--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -53,6 +53,20 @@ module.exports = {
         })
     },
 
+    getProductbyId: (req, res)=>{
+        const {id} = req.params
+        let sql = `SELECT p.product_id, p.image, p.price, p.product_name, c.category_id, c.category_name FROM ref_product_category pc
+        inner join tbl_product p
+        on p.product_id = pc.product_id
+        inner join tbl_category c
+        on c.category_id = pc.category_id
+        where p.product_id = ?`
+        db.query(sql,[id],(err, dataproduct)=>{
+            if (err) return res.status(500).send(err)
+            return res.status(200).send(dataproduct)
+        })
+    },
+
     getProductbyPage:(req,res)=>{
         const {page} = req.params
         let sql =`select * from tbl_product limit ${(page-1)*5},8`
