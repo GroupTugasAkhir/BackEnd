0c8b82688825700daafb50edd515ea5358d47154
hasianamin <hasianamin14@gmail.com>
2020-12-05 21:36:40 +0700

    add item to cart

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -2,7 +2,10 @@ const Router = require('express').Router()
 const {CartControllers} = require('./../controllers')
 
 Router.get('/getCart/:user_id',CartControllers.getCart)
+Router.post('/getTrx',CartControllers.userTrxOnCart)
 Router.post('/updateCart',CartControllers.updateCart)
 Router.post('/deleteCart',CartControllers.deleteCart)
+Router.get('/cartLength/:user_id',CartControllers.getCartLength)
+Router.get('/userCart/:user_id',CartControllers.userCartData)
 
 module.exports = Router
