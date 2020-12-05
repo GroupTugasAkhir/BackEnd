4a7f8ceabb4c42efbf74a877bfd3dfbcc963df3c
anggi <anggiprastianto30@gmail.com>
2020-12-05 19:16:36 +0700

    finalizing transcation controller

--- controllers/TransactionControllers.js
+++ controllers/TransactionControllers.js
@@ -1,16 +1,6 @@
 const { db } = require('./../connection')
-
-const queryProm = (sql) => {
-    return new Promise((resolve, reject)=> {
-        db.query(sql, (err, results)=> {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(results)
-            }
-        })
-    })
-}
+const fs = require('fs')
+const {uploader} = require('./../helpers/uploader')
 
 module.exports = {
     onpaycc: (req, res)=> {
@@ -42,7 +32,55 @@ module.exports = {
                 db.query(sql, updateTransData, (err)=> {
                     if (err) return res.status(500).send({message:err.message})
         
-                    return res.send('berhasil') //tidak perlu getcart lagi karena jika berhasil, di front otomatis kosong
+                    return res.send('succeed') //tidak perlu getcart lagi karena jika berhasil, di front otomatis kosong
+                })
+            })
+        })
+    },
+    onpayinvoice: (req, res)=> {
+        const path = '/invoice'
+        const upload = uploader(path, 'INVOICE').fields([{ name: 'invoice'}])
+
+        upload(req, res, (err)=> {
+            if(err) return res.status(500).json({message: 'upload picture failed !', error: err.message})
+            
+            console.log('invoice uploaded')
+            const { invoice } = req.files
+            const invoicePath =  invoice ? path + '/' + invoice[0].filename : null
+            console.log(invoicePath);
+
+            console.log(req.body.datainvoice);
+            const invoiceData = JSON.parse(req.body.datainvoice)
+
+            let idUser = {
+                notes: invoiceData.notes
+            }
+
+            let sql = `update tbl_user set ? where user_id = ${db.escape(invoiceData.user_id)}`
+            db.query(sql, [idUser], (err)=> {
+                if (err) return res.status(500).send({message:err.message})
+
+                sql = `select * from tbl_location where longitude = ? and latitude = ?`
+                db.query(sql, [invoiceData.matchLoc.longitude, invoiceData.matchLoc.latitude], (err, locationRes)=> {
+                    if (err) return res.status(500).send({message:err.message})
+
+                    sql = `update tbl_transaction set ? where transaction_id = ${db.escape(invoiceData.idtrans)}`
+                    let updateTransData = {
+                        date_in: Date.now(),
+                        status: 'onWaitingConfirm',
+                        payment_proof: invoicePath,
+                        location_id: locationRes[0].location_id
+                    }
+            
+                    db.query(sql, updateTransData, (err)=> {
+                        if (err) {
+                            fs.unlinkSync('./public'+invoicePath)
+                            res.status(500).send({message: err.message})
+                        }
+                        
+                        console.log('succeed');
+                        return res.send('succeed') //tidak perlu getcart lagi karena jika berhasil, di front otomatis kosong
+                    })
                 })
             })
         })
--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const {TransactionControllers} = require('./../controllers')
 
 Router.post('/onpaycc', TransactionControllers.onpaycc)
+Router.post('/onpayinvoice', TransactionControllers.onpayinvoice)
 
 module.exports = Router
