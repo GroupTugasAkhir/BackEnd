da32fea63339188c02ed7ef464be71e3cc4e71f5
Hannah <hannah4669@users.noreply.github.com>
2020-12-12 00:37:27 +0700

    aaa

--- controllers/UserOrdersControllers.js
+++ controllers/UserOrdersControllers.js
@@ -7,10 +7,14 @@ const fs = require('fs')
 module.exports = {
     getOrderDetails: (req, res) => {
         const {id} = req.params // user_id
-        let sql = `select td.*, p.product_name, p.image, t.status, t.user_id, t.location_id
+        let sql = `select distinct td.*, p.product_name, p.image, t.status, t.user_id, t.date_in,
+        lt.product_id as prod_id, lt.status as status_log
         from tbl_transaction_detail td join tbl_product p on td.product_id=p.product_id
         join tbl_transaction t on td.transaction_id=t.transaction_id
-        where t.user_id=${db.escape(id)} and status != 'onCart'`
+        left join tbl_log_transaction lt on lt.transaction_id=t.transaction_id
+        and lt.product_id=td.product_id and lt.status = 'completed'
+        where t.user_id=${db.escape(id)} and t.status != 'onCart' and t.status != 'completed';`
+
         db.query(sql, (err, dataOrdersUser)=>{
             if (err) return res.status(500).send({message:err.message})
             
@@ -72,7 +76,7 @@ module.exports = {
                     })
                 }
                     
-                    let sql = `select t.*, td.product_id, lt.product_id as prod_id, lt.status as status_log
+                    let sql = `select distinct t.*, td.product_id, lt.product_id as prod_id, lt.status as status_log
                     from tbl_transaction_detail td join tbl_transaction t on t.transaction_id=td.transaction_id
                     left join tbl_log_transaction lt on lt.transaction_id=t.transaction_id
                     and lt.product_id=td.product_id and lt.status = 'completed'
@@ -113,10 +117,13 @@ module.exports = {
                                         })
                                     }
 
-                                    let sql = `select td.*, p.product_name, p.image, t.status, t.user_id, t.location_id
+                                    let sql = `select distinct td.*, p.product_name, p.image, t.status, t.user_id,  t.date_in,
+                                    lt.product_id as prod_id, lt.status as status_log
                                     from tbl_transaction_detail td join tbl_product p on td.product_id=p.product_id
                                     join tbl_transaction t on td.transaction_id=t.transaction_id
-                                    where t.user_id=${db.escape(data.user_id)} and status != 'onCart'`
+                                    left join tbl_log_transaction lt on lt.transaction_id=t.transaction_id
+                                    and lt.product_id=td.product_id and lt.status = 'completed'
+                                    where t.user_id=${db.escape(id)} and t.status != 'onCart' and t.status != 'completed';`
                                     db.query(sql, (err, dataOrdersUser)=>{
                                         if (err) return res.status(500).send({message:err.message})
                                         
@@ -138,10 +145,13 @@ module.exports = {
                                 
                             })
                         }else{
-                            let sql = `select td.*, p.product_name, p.image, t.status, t.user_id, t.location_id
+                            let sql = `select distinct td.*, p.product_name, p.image, t.status, t.user_id,  t.date_in,
+                            lt.product_id as prod_id, lt.status as status_log
                             from tbl_transaction_detail td join tbl_product p on td.product_id=p.product_id
                             join tbl_transaction t on td.transaction_id=t.transaction_id
-                            where t.user_id=${db.escape(data.user_id)} and status != 'onCart'`
+                            left join tbl_log_transaction lt on lt.transaction_id=t.transaction_id
+                            and lt.product_id=td.product_id and lt.status = 'completed'
+                            where t.user_id=${db.escape(id)} and t.status != 'onCart' and t.status != 'completed';`
                             db.query(sql, (err, dataOrdersUser)=>{
                                 if (err) return res.status(500).send({message:err.message})
                                 
@@ -165,5 +175,45 @@ module.exports = {
             })
         })
 
-    }
+    },
+
+    //========================================= COMPLETED ORDER ========================================
+
+    getCompleted: (req, res) => {
+        const {id} = req.params // user_id
+        let sql = `select distinct td.*, p.product_name, p.image, t.status, t.user_id, t.date_in,
+        lt.product_id as prod_id, lt.status as status_log
+        from tbl_transaction_detail td join tbl_product p on td.product_id=p.product_id
+        join tbl_transaction t on td.transaction_id=t.transaction_id
+        left join tbl_log_transaction lt on lt.transaction_id=t.transaction_id
+        and lt.product_id=td.product_id and lt.status = 'completed'
+        where t.user_id=${db.escape(id)} and lt.status = 'completed';`
+
+        db.query(sql, (err, dataOrdersUser)=>{
+            if (err) return res.status(500).send({message:err.message})
+            
+            return res.status(200).send(dataOrdersUser)
+        })
+    },
+
+    getRating: (req, res) => {
+        const {idUs, idPr} = req.params // user_id, product_id
+        // const data = req.body
+        // console.log(idUs)
+        // console.log(data.IdPr)
+        let sql = `select c.*, p.product_name, p.image, u.username, u.photo
+        from tbl_comment c join tbl_user u on c.user_id=u.user_id
+        join tbl_product p on c.product_id=p.product_id
+        where c.user_id=${db.escape(idUs)} and c.product_id=${db.escape(idPr)}`
+
+        db.query(sql, (err, dataRating)=>{
+            if (err) return res.status(500).send({message:err.message})
+            if (!dataRating) {
+                return res.status(500).send({message: 'data rating tidak ada'})
+            }
+            
+            // console.log(dataRating)
+            return res.status(200).send(dataRating[0])
+        })
+    },
 }
--- routes/UserOrdersRoutes.js
+++ routes/UserOrdersRoutes.js
@@ -7,4 +7,8 @@ Router.get('/getOrders/:id', UserOrdersControllers.getOrderDetails)
 Router.get('/getCurrentOrder/:id', UserOrdersControllers.getSelectedOrder)
 Router.post('/completeOrder', UserOrdersControllers.completeOrder)
 
+// Completed order
+Router.get('/getCompleted/:id', UserOrdersControllers.getCompleted)
+Router.get('/getRating/:idUs/:idPr', UserOrdersControllers.getRating)
+
 module.exports = Router
